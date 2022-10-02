5dec1e4cf7d72e548fd7834dd61e6e98839c5418
Laibaax <sa20b057@technikum-wien.at>
2022-10-02 15:28:41 +0200

    Step 6: add keyboard event listener

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const button = document.getElementById("add");
 button.addEventListener("click", handleClick);
 
+const input = document.getElementById("text");
+input.addEventListener("keydown", handleKeyDown);
+
 function handleClick(/* event */) {
-  const input = document.getElementById("text");
-  const note = input.value;
+    add();
+}
+function handleKeyDown(event) {
+    if(event.key === "Enter") {
+        add();
+    }
+}
+
+function add() {
+    const input = document.getElementById("text");
+    const note = input.value;
+}
+
   if (note) {
     const list = document.getElementById("list");
     const item = document.createElement("li");
     item.textContent = note;
     list.appendChild(item);
     input.value = ""; //reset input
-    input.focus();  
+    input.focus();  //focus  
   }
-}
